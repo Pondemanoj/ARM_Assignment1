ce33d3b14f6ecf71d6da1dd599778ffa9a56ad80
Pondemanoj <pondemanoj@gmail.com>
2021-08-16 12:57:18 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,3 +1,7 @@
+ /* 1. Write a C program to read three Whole numbers (what we call as integers in programming language) and
+implement the algorithm, to find the largest of these three  */
+
+
 #include<stdio.h>
 
 main()
